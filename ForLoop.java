eca34908a17d09cb37494398938fe6d4247f0499
Anogna-Ch <78353061+Anogna-Ch@users.noreply.github.com>
2022-12-05 13:38:22 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+package com.fullstackdeveloper;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i = 10; i >= 1; i --) {
+			System.out.println("Value of i: " + i);
+			//System.out.println(i);
+			
+		}
+		
+		/*for(;;) {
+			System.out.println();
+		}*/
+		
+	}
+
+}
